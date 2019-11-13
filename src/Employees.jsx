5e3b613e9806453edf63bbaca61659ffdfdd80ea
HubertNastaly <h.nastaly@gmail.com>
2019-11-13 22:56:21 +0100

    Lab6Ex4 error 404 while deleting

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -5,11 +5,20 @@ class Employees extends React.Component
 {
   constructor(props) {
     super(props);
-    this.state = {isLoading: true, employees: null};
+    this.state = {
+      isLoading: true, 
+      employees: null,
+      showForm: false,
+      isSaving: false,
+      deletedEmployee: null,
+      selectedEmployee: null
+    };
     this.showForm = this.showForm.bind(this);
     this.hideForm = this.hideForm.bind(this);
     this.saveEmployee = this.saveEmployee.bind(this);
     this.getEmployees = this.getEmployees.bind(this);
+    this.selectEmployee = this.selectEmployee.bind(this);
+    this.deleteEmployee = this.deleteEmployee.bind(this);
   }
   componentDidMount(){
     return this.getEmployees();
@@ -17,13 +26,20 @@ class Employees extends React.Component
   getEmployees(){
     return fetch('http://localhost:3000/employees')
       .then(response => response.json())
-      .then(e => this.setState({ employees: e, isLoading: false, showForm: false }));
+      .then(e => this.setState({ 
+        employees: e, 
+        isLoading: false, 
+        showForm: false,
+        selectedEmployee: null }));
   }
   showForm(){
     this.setState({showForm: true});
   }
   hideForm(){
     this.setState({showForm: false});
+  }
+  selectEmployee(e){
+    this.setState({selectedEmployee: e.target.value});
   }
   saveEmployee(e){
     console.log(e.target);
@@ -34,8 +50,6 @@ class Employees extends React.Component
     //new id is max id from all employees + 1
     // const startingVal = this.state.employees[0]._id;
     // const id = this.state.employees.reduce((max,curr) => curr._id > max ? curr._id : max, startingVal);
-
-    
 
     const form = e.target;
     const name = form.elements['name'].value;
@@ -51,6 +65,8 @@ class Employees extends React.Component
       email: email
     }
     const newEmployeeStringified = JSON.stringify(newEmployee);
+
+    //some hash function from internet
     const generateId = () => {
       var str = newEmployeeStringified;
       var hash = 0;
@@ -58,13 +74,11 @@ class Employees extends React.Component
       for (let i = 0; i < str.length; i++) {
           const char = str.charCodeAt(i);
           hash = ((hash<<5)-hash)+char;
-          hash = hash & hash; // Convert to 32bit integer
+          hash = hash & hash; 
       }
       return Math.abs(hash);
-  }
-    console.log(generateId());
+    }
     newEmployee.id = parseInt(generateId());
-    console.log(newEmployee);
     fetch('http://localhost:3000/employees', {
       method: 'POST', 
       headers: {
@@ -77,15 +91,34 @@ class Employees extends React.Component
       if (resp.ok) {
           return resp.json()
       } else {
-          throw new Error("Wystąpił błąd połączenia!")
+          throw new Error("Connection error!")
       }
     })
-    .catch(error => console.dir("Błąd: ", error));
+    .catch(error => console.dir("Error: ", error));
 
     this.setState({isSaving: false, isLoading: true});
 
     this.getEmployees();
   }
+
+  deleteEmployee(){
+    const id = this.state.selectedEmployee;
+    if(!id)
+    {
+      return;
+    }
+
+    this.setState({deletedEmployee: id});
+    fetch('http://localhost:3000/employees/:'+id, {
+      method: 'DELETE',
+      headers: {'content-type': 'application/json'},
+    });
+
+    this.setState({deletedEmployee: null});
+
+    this.getEmployees();
+  }
+
   render(){
     return(
       <div>
@@ -103,18 +136,28 @@ class Employees extends React.Component
           </thead>
           <tbody key="tbody">
             {this.state.employees.map((employee) => 
-              <tr key={"tr"+employee._id}>
-                <th key={employee._id}>{employee._id}</th>
-                <th key={employee._id + employee.isActive}>{employee.isActive ? 1 : 0}</th>
-                <th key={employee._id + employee.age}>{employee.age}</th>
-                <th key={employee._id + employee.name}>{employee.name}</th>
-                <th key={employee._id + employee.company}>{employee.company}</th>
-                <th key={employee._id + employee.email}>{employee.email}</th>
-              </tr>
+              
+              this.state.deletedEmployee ? <tr>Deleting...</tr> :
+
+                <tr key={"tr"+employee._id}>
+                  <th key={employee._id}>
+                    <label>
+                      <input key={"radio"+employee._id} type="radio" name="selectEmployee" value={employee._id} onInput={this.selectEmployee}></input>
+                      {employee._id}
+                    </label>
+                  </th>
+                  <th key={employee._id + employee.isActive}>{employee.isActive ? 1 : 0}</th>
+                  <th key={employee._id + employee.age}>{employee.age}</th>
+                  <th key={employee._id + employee.name}>{employee.name}</th>
+                  <th key={employee._id + employee.company}>{employee.company}</th>
+                  <th key={employee._id + employee.email}>{employee.email}</th>
+                </tr>
+              
             )}
           </tbody>
           </table>
         }
+        <button onClick={this.deleteEmployee}>Delete selected employee</button>
         <button onClick={this.showForm}>Add new employee</button>
         {this.state.showForm ? this.state.isSaving ? <p>Saving ...</p> : <AddEmployeeForm saveEmployee={this.saveEmployee} hideForm={this.hideForm}/> : null}
       </div>
