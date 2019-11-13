0a972df2584d2a248727bad1871bfd50bda9b9c1
HubertNastaly <h.nastaly@gmail.com>
2019-11-13 20:01:05 +0100

    Lab6Ex3 posting without id

--- src/AddEmployeeForm.jsx
+++ src/AddEmployeeForm.jsx
@@ -4,13 +4,12 @@ export default class AddEmployeeForm extends React.Component
 {
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {};
   }
   render(){
     return (
-        <form>
-        <label><input type="checkbox" name="isActive" onSubmit={this.props.saveEmployee}></input>is active</label>
+        <form onSubmit={event => { event.persist(); this.props.saveEmployee(event)}}>
+        <label><input type="checkbox" name="isActive"></input>is active</label>
         <br/>
         <input type="number" name="age" placeholder="Age"></input>
         <br/>
--- src/Employees.jsx
+++ src/Employees.jsx
@@ -9,8 +9,12 @@ class Employees extends React.Component
     this.showForm = this.showForm.bind(this);
     this.hideForm = this.hideForm.bind(this);
     this.saveEmployee = this.saveEmployee.bind(this);
+    this.getEmployees = this.getEmployees.bind(this);
   }
   componentDidMount(){
+    return this.getEmployees();
+  }
+  getEmployees(){
     return fetch('http://localhost:3000/employees')
       .then(response => response.json())
       .then(e => this.setState({ employees: e, isLoading: false, showForm: false }));
@@ -21,65 +25,98 @@ class Employees extends React.Component
   hideForm(){
     this.setState({showForm: false});
   }
-  async saveEmployee(e){
+  saveEmployee(e){
+    console.log(e.target);
     e.preventDefault();
-    console.log("in saveEmployee");
-    // this.setState({isSaving: true});
-    // try {
-    //   const response = await fetch('localhost:3000/employees', {
-    //     method: 'POST', 
-    //     body: JSON.stringify(e.target.value),
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     }
-    //   });
-    //   const json = await response.json();
-    //   console.log('Success:', JSON.stringify(json));
-    // } 
-    // catch (error) {
-    //   console.error('Error:', error);
-    // }
-    // this.setState({isSaving: false});
+    
+    this.setState({isSaving: true});
+
+    //new id is max id from all employees + 1
+    // const startingVal = this.state.employees[0]._id;
+    // const id = this.state.employees.reduce((max,curr) => curr._id > max ? curr._id : max, startingVal);
+
+    
+
+    const form = e.target;
+    const name = form.elements['name'].value;
+    const email = form.elements['email'].value;
+    const company = form.elements['company'].value;
+    const age = form.elements['age'].value;
+    const isActive = form.elements['isActive'].value == "on" ? true : false;
+    const newEmployee = {
+      isActive: isActive,
+      age: age,
+      name: name,
+      company: company,
+      email: email
+    }
+    const newEmployeeStringified = JSON.stringify(newEmployee);
+    const generateId = () => {
+      var str = newEmployeeStringified;
+      var hash = 0;
+      if (str.length == 0) return hash;
+      for (let i = 0; i < str.length; i++) {
+          const char = str.charCodeAt(i);
+          hash = ((hash<<5)-hash)+char;
+          hash = hash & hash; // Convert to 32bit integer
+      }
+      return Math.abs(hash);
+  }
+    console.log(generateId());
+    newEmployee.id = parseInt(generateId());
+    console.log(newEmployee);
+    fetch('http://localhost:3000/employees', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newEmployee)
+    })
+    .then(resp => {
+      if (resp.ok) {
+          return resp.json()
+      } else {
+          throw new Error("Wystąpił błąd połączenia!")
+      }
+    })
+    .catch(error => console.dir("Błąd: ", error));
+
+    this.setState({isSaving: false, isLoading: true});
+
+    this.getEmployees();
   }
   render(){
-    if(this.state.isSaving){
-      return(
-        <p>isSaving...</p>
-      )
-    }
-    if(this.state.isLoading){
-      return(
-        <p>isLoading...</p>
-      )
-    }
     return(
       <div>
-        <table style={{fontSize: 12, fontWeight: 'normal'}}>
-        <thead>
-          <tr>
-              <th scope="col">Id</th>
-              <th scope="col">is Active</th>
-              <th scope="col">Age</th>
-              <th scope="col">Name</th>
-              <th scope="col">Company</th>
-              <th scope="col">Email</th>
-          </tr>
-        </thead>
-        <tbody key="tbody">
-          {this.state.employees.map((employee) => 
-            <tr key={"tr"+employee._id}>
-              <th key={employee._id}>{employee._id}</th>
-              <th key={employee._id + employee.isActive}>{employee.isActive ? 1 : 0}</th>
-              <th key={employee._id + employee.age}>{employee.age}</th>
-              <th key={employee._id + employee.name}>{employee.name}</th>
-              <th key={employee._id + employee.company}>{employee.company}</th>
-              <th key={employee._id + employee.email}>{employee.email}</th>
+        {this.state.isLoading ? <p>Loading...</p> :
+          <table style={{fontSize: 12, fontWeight: 'normal'}}>
+          <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">is Active</th>
+                <th scope="col">Age</th>
+                <th scope="col">Name</th>
+                <th scope="col">Company</th>
+                <th scope="col">Email</th>
             </tr>
-          )}
-        </tbody>
-        </table>
+          </thead>
+          <tbody key="tbody">
+            {this.state.employees.map((employee) => 
+              <tr key={"tr"+employee._id}>
+                <th key={employee._id}>{employee._id}</th>
+                <th key={employee._id + employee.isActive}>{employee.isActive ? 1 : 0}</th>
+                <th key={employee._id + employee.age}>{employee.age}</th>
+                <th key={employee._id + employee.name}>{employee.name}</th>
+                <th key={employee._id + employee.company}>{employee.company}</th>
+                <th key={employee._id + employee.email}>{employee.email}</th>
+              </tr>
+            )}
+          </tbody>
+          </table>
+        }
         <button onClick={this.showForm}>Add new employee</button>
-        {this.state.showForm ? <AddEmployeeForm saveEmployee={this.saveEmployee} hideForm={this.hideForm}/> : null}
+        {this.state.showForm ? this.state.isSaving ? <p>Saving ...</p> : <AddEmployeeForm saveEmployee={this.saveEmployee} hideForm={this.hideForm}/> : null}
       </div>
     )
   }
